08fa17d266ee67d90d22d8f4da91cbe10b89d84f
Sztakler <krystian.jasionek@gmail.com>
2022-05-17 10:24:35 +0200

    Changed order of input validation in Transport class constructor.

--- src/transport.cpp
+++ src/transport.cpp
@@ -20,16 +20,6 @@ Transport::Transport(int argc, char *argv[]) {
   std::cout << argc << " " << ip_address_string << " " << port << " "
             << filename << " " << size << "\n";
 
-  int result = inet_pton(AF_INET, ip_address_string.c_str(), &ipv4_address);
-  if (result == 0) {
-    fprintf(stderr,
-            "<inet_pton>: src does not contain a character string representing "
-            "a valid network address in the specified address family.\n");
-    exit(EXIT_FAILURE);
-  } else if (result == -1) {
-    perror("<inet_pton>");
-    exit(EXIT_FAILURE);
-  }
 
   if (port < 1024 || port > 65535) {
     fprintf(stderr,
@@ -43,6 +33,17 @@ Transport::Transport(int argc, char *argv[]) {
             "Size is too large. Enter number between 0-10000000 bytes (entered "
             "%d).\n",
             size);
+    exit(EXIT_FAILURE);
+  }
+
+  int result = inet_pton(AF_INET, ip_address_string.c_str(), &ipv4_address);
+  if (result == 0) {
+    fprintf(stderr,
+            "<inet_pton>: src does not contain a character string representing "
+            "a valid network address in the specified address family.\n");
+    exit(EXIT_FAILURE);
+  } else if (result == -1) {
+    perror("<inet_pton>");
     exit(EXIT_FAILURE);
   }
 }
